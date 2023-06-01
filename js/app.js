f733eff2808530e791c82dd8aef3267bdbab5e79
fmarvass <134193821+fmarvass@users.noreply.github.com>
2023-06-01 01:22:14 -0300

    a

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ check.addEventListener("click", idioma);
 function idioma() {
   let id = check.checked;
   if (id === true) {
-    location.href = "es/index.html";
+    location.href = "es/indexES.html";
   } else{
     location.href = "../index.html";
   }
