c82e1af29bf0c748f9a03120d4fc2f9b5dc54f95
fmarvass <134193821+fmarvass@users.noreply.github.com>
2026-06-08 18:54:43 -0300

    Se activa el motor

--- js/main.js
+++ js/main.js
@@ -28,17 +28,17 @@
     });
 
 
-    // // Typed Initiate
-    // if ($('.typed-text-output').length == 1) {
-    //     var typed_strings = $('.typed-text').text();
-    //     var typed = new Typed('.typed-text-output', {
-    //         strings: typed_strings.split(', '),
-    //         typeSpeed: 100,
-    //         backSpeed: 20,
-    //         smartBackspace: false,
-    //         loop: true
-    //     });
-    // }
+    // Typed Initiate (ACTIVADO OPERATIVO)
+    if ($('.typed-text-output').length == 1) {
+        var typed_strings = $('.typed-text').text();
+        var typed = new Typed('.typed-text-output', {
+            strings: typed_strings.split(', '),
+            typeSpeed: 100,
+            backSpeed: 20,
+            smartBackspace: false,
+            loop: true
+        });
+    }
 
 
     // Modal Video
@@ -67,14 +67,6 @@
             $('.scroll-to-bottom').fadeIn('slow');
         }
     });
-
-
-    // // Skills
-    // $('.skill').waypoint(function () {
-    //     $('.progress .progress-bar').each(function () {
-    //         $(this).css("width", $(this).attr("aria-valuenow") + '%');
-    //     });
-    // }, {offset: '80%'});
 
 
     // Portfolio isotope and filter
